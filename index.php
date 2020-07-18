d0a4a33c6e3a2b7e3e97e8f44d7c194b5bdd040d
Antulio91 <Antulio91@users.noreply.github.com>
2020-07-18 12:57:13 -0430

    Actualizando repositorio
    borrando comentarios

--- index.php
+++ index.php
@@ -1,45 +1,4 @@
 <?php
-
-	/*
-		Realizar un json de Peliculas Agrupados por Categorías, en base al archivo movies.json, calcular la cantidad de peliculas por categorias y el promedio en minutos.
-
-		Ejemplo:
-		{
-			"Comedy": {
-		        "total_movies": 2,
-		        "average_minutes": 43,
-		        "total_minutes": 86,
-		        "movies": [
-		            {
-		                "title": "Vikings",
-		                "runtime": 44,
-		                "year": "2013\u2013",
-		                "director": "N\/A",
-		                "writer": "Michael Hirst",
-		                "actors": "Travis Fimmel, Clive Standen, Gustaf Skarsg\u00e5rd, Katheryn Winnick",
-		                "plot": "The world of the Vikings is brought to life through the journey of Ragnar Lothbrok, the first Viking to emerge from Norse legend and onto the pages of history - a man on the edge of myth.",
-		                "lenguage": "English, Old English, Norse, Old, Latin"
-		            },
-		            {
-		                "title": "Gotham",
-		                "runtime": 42,
-		                "year": "2014\u2013",
-		                "director": "N\/A",
-		                "writer": "Bruno Heller",
-		                "actors": "Ben McKenzie, Donal Logue, David Mazouz, Sean Pertwee",
-		                "plot": "The story behind Detective James Gordon's rise to prominence in Gotham City in the years before Batman's arrival.",
-		                "lenguage": "English"
-		            }
-		        ]
-		    }
-	 	}
-	 	######################
-			Postulante:
-			Fecha:
-			Telefono:
-			Corre: 
-		######################
-	*/
 	header('Content-type: application/json;');
 	$url = './movies.json'; // path to your JSON file
 	$data = file_get_contents($url); // put the contents of the file into a variable
@@ -64,9 +23,6 @@
 			array_push($newArray[$genre]["movies"], $movie);
 		};
 	};
-
-	/*Ïmprime contenido del Json*/
-	#print_r(json_encode($movies, JSON_PRETTY_PRINT));
 
 	/*Ïmprime contenido del nuevo Array*/
 	print_r(json_encode($newArray, JSON_PRETTY_PRINT));
